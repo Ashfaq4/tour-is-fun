3765b9e5b5d10643704572ab25180b3d5bbd0241
Ashfaq <ashfaqctg74@gmail.com>
2018-07-29 12:20:24 +0600

    Commited all folders

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,28 @@
+var gulp        = require('gulp');
+    watch       = require('gulp-watch'),
+    browsersync = require('browser-sync').create();
+    
+gulp.task('watch', function () {
+  //Automatic Browser Refreshing
+  browsersync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  //Watching changing in html file
+  watch('./app/index.html', function () {
+    browsersync.reload();
+  });
+
+  //Watching changing in sytle folder
+  watch('./app/assets/styles/**/*.css', function () {
+    gulp.start('cssInject');
+  });
+});
+
+gulp.task('cssInject', ['styles'], function () {
+  return gulp.src('./app/temp/styles/style.css')
+    .pipe(browsersync.stream());
+});
